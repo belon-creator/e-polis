a45a91b7a94066d0f352c86e6da523a8e42f2aa2
belon-creator <belon8911@gmail.com>
2024-11-01 12:04:20 +0200

    chng accordion

--- js/index.js
+++ js/index.js
@@ -5,21 +5,26 @@ const accordionItems = document.querySelectorAll(".accordion-item");
 accordionItems.forEach((item) => {
   const header = item.querySelector(".accordion-header");
   const content = item.querySelector(".accordion-content");
+  const icon = item.querySelector(".accordion-icon");
 
   header.addEventListener("click", () => {
-    // Закриваємо всі вмістові блоки
+    // Закриваємо всі вмістові блоки і змінюємо іконку
     accordionItems.forEach((i) => {
       const itemContent = i.querySelector(".accordion-content");
+      const itemIcon = i.querySelector(".accordion-icon");
       if (itemContent !== content) {
         itemContent.style.display = "none";
+        itemIcon.textContent = "+";
       }
     });
 
     // Перевіряємо стан видимості вмісту і змінюємо його стан
     if (content.style.display === "block") {
       content.style.display = "none";
+      icon.textContent = "+";
     } else {
       content.style.display = "block";
+      icon.textContent = "-";
     }
   });
 });
