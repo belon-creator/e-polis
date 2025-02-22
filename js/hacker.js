f31f5d2bc8d08daeb0ce01d03a5003868afbda64
belon-creator <belon8911@gmail.com>
2025-02-22 16:41:12 +0200

    ads

--- js/hacker.js
+++ js/hacker.js
@@ -1,16 +1,18 @@
 document.addEventListener('contextmenu', event => {
+  event.preventDefault();
+  alert('хацкєр? 😑');
+});
+
+document.addEventListener('keydown', event => {
+  if (
+    (event.ctrlKey && (event.key === 'u' || event.key === 'U')) || // Ctrl + U
+    event.key === 'F12' || // F12
+    (event.ctrlKey && event.shiftKey && (event.key === 'I' || event.key === 'i')) || // Ctrl + Shift + I
+    (event.ctrlKey && (event.key === 'г' || event.key === 'Г')) || // Ctrl + Г
+    (event.ctrlKey && event.shiftKey && (event.key === 'ш' || event.key === 'Ш')) // Ctrl + Shift + Ш
+  ) {
     event.preventDefault();
     alert('хацкєр? 😑');
-  });
-  
-  document.addEventListener('keydown', event => {
-    if (
-      event.ctrlKey && (event.key === 'u' || event.key === 'U') || // Ctrl + U
-      event.key === 'F12' || // F12
-      (event.ctrlKey && event.shiftKey && (event.key === 'I' || event.key === 'i')) // Ctrl + Shift + I
-    ) {
-      event.preventDefault();
-      alert('хацкєр? 😑');
-    }
-  });
+  }
+});
   
