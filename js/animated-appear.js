78562a031f7e5acecde69dd7115925d899a01439
belon-creator <belon8911@gmail.com>
2025-07-04 14:53:57 +0300

    frame design

--- js/animated-appear.js
+++ js/animated-appear.js
@@ -1,26 +1,53 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const animatedElements = document.querySelectorAll('.animated-element');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const animatedElements = document.querySelectorAll('.animated-element');
 
-  const observer = new IntersectionObserver(
-    entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('visible');
-        }
-      });
-    },
-    {
-      threshold: 0.1, // спрацьовує, коли 10% елемента в полі зору
-    },
-  );
+//   const observer = new IntersectionObserver(
+//     entries => {
+//       entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//           entry.target.classList.add('visible');
+//         }
+//       });
+//     },
+//     {
+//       threshold: 0.1, // спрацьовує, коли 10% елемента в полі зору
+//     },
+//   );
 
-  animatedElements.forEach(el => observer.observe(el));
-});
+//   animatedElements.forEach(el => observer.observe(el));
+// });
 
-//   =============================
+// //   =============================
 
-document.addEventListener('DOMContentLoaded', function () {
-  const elements = document.querySelectorAll('.fade-in-left');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const elements = document.querySelectorAll('.fade-in-left');
+
+//   const observer = new IntersectionObserver(
+//     entries => {
+//       entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//           entry.target.classList.add('visible');
+//         }
+//       });
+//     },
+//     {
+//       threshold: 0.1,
+//     },
+//   );
+
+//   elements.forEach(el => observer.observe(el));
+// });
+
+// // ==================================
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   document.querySelectorAll('.fade-in-right').forEach(el => {
+//     el.classList.add('visible');
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = document.querySelectorAll('.fade-in-left, .fade-in-right, .animated-element');
 
   const observer = new IntersectionObserver(
     entries => {
@@ -36,12 +63,4 @@ document.addEventListener('DOMContentLoaded', function () {
   );
 
   elements.forEach(el => observer.observe(el));
-});
-
-// ==================================
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.fade-in-right').forEach(el => {
-    el.classList.add('visible');
-  });
 });
